f50fe0d616bd490b41e59db0b56af4c4ccc601f8
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2025-11-01 20:57:33 +0100

    Make wasm3 compile with Zig 0.15.2

--- build.zig
+++ build.zig
@@ -6,17 +6,20 @@ pub fn build(b: *std.Build) !void {
 
     const libm3_only = b.option(bool, "libm3", "Build libwasm3 only") orelse false;
 
-    const libwasm3 = b.addStaticLibrary(.{
+    const libwasm3 = b.addLibrary(.{
+        .linkage = .static,
         .name = "m3",
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .sanitize_c = .off, // fno-sanitize=undefined
+        }),
     });
-    libwasm3.root_module.sanitize_c = false; // fno-sanitize=undefined
-    libwasm3.defineCMacro("d_m3HasTracer", null);
+    libwasm3.root_module.addCMacro("d_m3HasTracer", "1");
 
-    if (libwasm3.rootModuleTarget().isWasm()) {
+    if (libwasm3.rootModuleTarget().cpu.arch.isWasm()) {
         if (libwasm3.rootModuleTarget().os.tag == .wasi) {
-            libwasm3.defineCMacro("d_m3HasWASI", null);
+            libwasm3.root_module.addCMacro("d_m3HasWASI", "1");
             libwasm3.linkSystemLibrary("wasi-emulated-process-clocks");
         }
     }
@@ -40,7 +43,7 @@ pub fn build(b: *std.Build) !void {
             "source/m3_module.c",
             "source/m3_parse.c",
         },
-        .flags = if (libwasm3.rootModuleTarget().isWasm())
+        .flags = if (libwasm3.rootModuleTarget().cpu.arch.isWasm())
             &cflags ++ [_][]const u8{
                 "-Xclang",
                 "-target-feature",
@@ -56,12 +59,16 @@ pub fn build(b: *std.Build) !void {
     if (!libm3_only) {
         const wasm3 = b.addExecutable(.{
             .name = "wasm3",
-            .target = target,
-            .optimize = optimize,
+            .root_module = b.createModule(.{
+                .target = target,
+                .optimize = optimize,
+            }),
         });
+
         for (libwasm3.root_module.include_dirs.items) |dir| {
             wasm3.addIncludePath(dir.path);
         }
+
         wasm3.addCSourceFile(.{
             .file = .{ .cwd_relative = "platforms/app/main.c" },
             .flags = &cflags,
